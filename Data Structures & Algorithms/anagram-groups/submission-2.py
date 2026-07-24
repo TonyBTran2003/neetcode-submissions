0dd0dec2bf411522a110655da2186de3988c4bfd
TonyBTran2003 <tonyiscoding2003@gmail.com>
2026-07-24 00:45:30 -0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        masterDict = {}
+        
+        for s in strs:
+            key = "".join(sorted(s))
+
+            if key not in masterDict:
+                masterDict[key] = []
+
+            masterDict[key].append(s)
+
+        return list(masterDict.values())
