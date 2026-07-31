7dbd03f73a6e4b45fa44d0e63860bbe4a7fa2994
TonyBTran2003 <tonyiscoding2003@gmail.com>
2026-07-31 01:36:08 -0700

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        sDict = {}
+
+        for char in s:
+            sDict[char] = sDict.get(char, 0) + 1
+
+        for char in t:
+            if char not in sDict:
+                return False
+            sDict[char] -= 1
+
+        return all(count == 0 for count in sDict.values())
+
+        
