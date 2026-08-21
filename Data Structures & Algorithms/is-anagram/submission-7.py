2548e60235aa9ba0302907434e2126bf8706042b
TonyBTran2003 <tonyiscoding2003@gmail.com>
2026-08-21 14:33:37 -0700

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_dict = {}
+
+        for char in s:
+            s_dict[char] = s_dict.get(char, 0) + 1
+
+        for char in t:
+            if char not in s_dict:
+                return False
+            s_dict[char] -= 1
+
+        return all(s_dict[char] == 0 for char in s_dict)
